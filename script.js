a4766e9fc5dace18f957dbe5be46a3fb4976e621
Surya Teja Yellaboyina <yellaboinasuryateja@gmail.com>
2025-11-04 16:14:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ form.addEventListener('submit', async function (e) {
   };
 
   try {
-    const response = await fetch('https://surya-portfolio-backend.onrender.com', {
+    const response = await fetch('https://surya-portfolio-backend.onrender.com/api/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
